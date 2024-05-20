732c1b538c7b69278560aa823e0fec6e29f05273
Gonzalo-Figueroa <gfigueroa.digital@gmail.com>
2024-05-20 19:50:49 +0000

    Añadir archivo setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(
+    name='saludo',
+    version='0.1',
+    py_modules=['saludo'],
+    description='Un paquete simple que contiene una clase Saludo',
+    author='Gonzalo Figueroa',
+    author_email='gfigueroa.digital@gmail.com',
+    url='https://github.com/Gonzalo-Figueroa/ACHS',
+)
+    
